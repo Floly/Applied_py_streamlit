35be9baeba68580159487a3e08777997f583be80
Flops <ivan.esipoff@mail.ru>
2023-12-09 14:52:10 +0300

    asd

--- 2_streamlit.py
+++ 2_streamlit.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('datasets/D_basic.csv', index_col=0)
+first_line = 'Hello! This is my first streamlit project :balloon:'
 
+st.markdown(body=first_line)
 st.dataframe(data=df.head(4))

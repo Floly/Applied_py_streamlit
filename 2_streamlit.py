56596ae1ad14751deded7adbed0fec6ffec7fb51
Flops <ivan.esipoff@mail.ru>
2023-12-09 15:33:47 +0300

    asda

--- 2_streamlit.py
+++ 2_streamlit.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
-# import plotly.express as px
-import polars as pl
+import plotly.express as px
+# import polars
 import seaborn as sns
 
-# df = pd.read_csv('datasets/D_basic.csv', index_col=0)
-# first_line = '**Hello!** This is my first streamlit project :balloon:'
-# target_distribution = px.histogram(df['TARGET'])
+df = pd.read_csv('datasets/D_basic.csv', index_col=0)
+first_line = '**Hello!** This is my first streamlit project :balloon:'
+target_distribution = px.histogram(df['TARGET'])
 
 
-# st.markdown(body=first_line)
-# st.dataframe(data=df.head(4))
-# st.plotly_chart(target_distribution)
+st.markdown(body=first_line)
+st.dataframe(data=df.head(4))
+st.plotly_chart(target_distribution)

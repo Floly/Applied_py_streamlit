71a7eb4fc32e171e2448d90cf6c61b6d6177f1d2
Flops <ivan.esipoff@mail.ru>
2023-12-09 17:54:47 +0300

    Аналитика вклада в модель

--- 2_streamlit.py
+++ 2_streamlit.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-# import polars
+import statsmodels
 import seaborn as sns
 
 df = pd.read_csv('datasets/D_basic.csv', index_col=0)
-first_line = '**Hello!** This is my first streamlit project :balloon:'
-target_distribution = px.histogram(df['TARGET'])
-
 
 st.markdown(body=first_line)
 
@@ -22,15 +19,27 @@ with st.expander("Распределения признаков"):
                         title=f'{col} DISTRIBUTION')
         st.plotly_chart(fig)
 
+st.markdown("### Посмотрим на описательные статистики")
+st.dataframe(data=df.describe())
+
 st.markdown("### Посмотрим на матрицу корреляций")
 corr_fig = px.imshow(df.iloc[:,2:].corr().round(2), text_auto=True)
 st.plotly_chart(corr_fig)
 
-# with st.expander("Распределения признаков"):
-#     for col in cols:
-#         fig = px.histogram(df[col],
-#                         title=f'{col} DISTRIBUTION')
-#         st.plotly_chart(fig)
 
-st.markdown("### Посмотрим на описательные статистики")
-st.dataframe(data=df.describe())
+Xtrain = df[feats]
+ytrain = df[target]
+log_reg = sm.Logit(ytrain, Xtrain).fit() 
+log_reg.summary().tables[0]
+
+message = '''
+    ### Попробуем выявить зависимости между признаками и таргетом
+    Для этого построим логрег и посмотрим на значимость переменных
+    '''
+st.markdown(message)
+st.markdown('Качество получившейся модели')
+st.dataframe(log_reg.summary().tables[0])
+
+st.markdown('Влияние признаков на таргета')
+st.dataframe(log_reg.summary().tables[1])
+

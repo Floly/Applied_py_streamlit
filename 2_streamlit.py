4ce7d7caeaed066e5c83208932a9a09c424c1ce9
Flops <ivan.esipoff@mail.ru>
2023-12-09 14:58:04 +0300

    asdas

--- 2_streamlit.py
+++ 2_streamlit.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import seaborn as sns
 
 df = pd.read_csv('datasets/D_basic.csv', index_col=0)
-first_line = 'Hello! This is my first streamlit project :balloon:'
+first_line = '**Hello!** This is my first streamlit project :balloon:'
+target_distribution = px.histogram(df['TARGET'])
+
 
 st.markdown(body=first_line)
 st.dataframe(data=df.head(4))
+st.image(target_distribution)
